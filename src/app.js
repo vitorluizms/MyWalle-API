510788fb11ac2192d93e28859c0c64a028f9ecb6
vitorluizms <vitor.luiz.eer@gmail.com>
2023-07-10 13:24:37 -0300

    chore: adjust code for deploy on render

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 app.use(router);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`O servidor está rodando na porta ${PORT}`);
 });
