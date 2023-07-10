a84189826a9d03c315f4fefa754a15529f3b7fa5
vitorluizms <vitor.luiz.eer@gmail.com>
2023-07-10 16:39:20 -0300

    Correção no parâmetro de rota

--- src/schemas/transations.schema.js
+++ src/schemas/transations.schema.js
@@ -2,7 +2,7 @@ import Joi from "joi";
 
 const schemaTransation = Joi.object({
   value: Joi.number().precision(2).required(),
-  type: Joi.string().valid(":saida", ":entrada").required(),
+  type: Joi.string().valid("saida", "entrada").required(),
   description: Joi.string().required(),
 });
 
